fada23a92f8360a3b5b0e916553b07d412768a2f
GkingOficial <talisoncaedoso@gmail.com>
2020-11-05 16:33:07 +0000

    Colunas OK

--- test/main.c
+++ test/main.c
@@ -3,18 +3,21 @@
 
 int main(){
   FILE *f = fopen("file.csv", "r");
-  int row = 0, column = 0;
+  int rows = 0, columns = 1;
   char c;
   
-  while(c = getchar(f), !feof(f)){
-    if(ch == ","){
-      row++;
-    }
-
-    if(ch == '\n'){
-      column++;
+  while(c = getc(f), c != '\n'){
+    if(c == ','){
+      columns++;
     }
   }
+  
+  while(c = getc(f), !feof(f)){
+    if(c == '\n'){
+      rows++;
+    }
+    //printf("*%c, ", c);
+  }
 
-  printf("Rows: %d\nColumns: %d\n", rows, column);
+  printf("\nRows: %d\nColumns: %d\n", rows, columns);
 }
