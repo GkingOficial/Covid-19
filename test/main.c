653be9de7216d8fcce38d5a64feb15e71bee8a8c
DipperCurioso <danielalencar746@gmail.com>
2020-11-05 16:04:50 +0000

    Testando getch para contagem de rows e columns

--- test/main.c
+++ test/main.c
@@ -2,7 +2,19 @@
 #include <stdlib.h>
 
 int main(){
+  FILE *f = fopen('file.csv', 'r');
+  int row = 0, column = 0;
   char c;
-  FILE *f = 
-  while(c = getch(file) != '\n');
+  
+  while(c = getchar(f), !feof(f)){
+    if(ch == ","){
+      row++;
+    }
+
+    if(ch == '\n'){
+
+    }
+  }
+
+  printf("Rows: %d\nColumns: %d\n", rows, column);
 }
