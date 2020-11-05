78b0e0dd3ff6c65e5ccee3352c4916429aef832e
Daniel-Alencar <danielalencar746@gmail.com>
2020-11-05 11:40:56 -0300

    Adicionado forma de capturar a quantidade de rows e columns do CSV e atribui-lo ao TAD

--- Libs/csv lib/csv reader.c
+++ Libs/csv lib/csv reader.c
@@ -22,38 +22,38 @@ int main(){
 
 void readCSV(FILE *f, CSV *c){
     char texto[100], *word;
-    int i, j, column = 0, row = 0;
+    int i, j, column, row;
     
     c -> array = NULL;
     c -> array = (char ***)malloc(sizeof(char **) * 4);
     for(i=0;i<4;i++) {
         c->array[i] = (char **)malloc(sizeof(char *) * 3);
-        for(j=0;j<3;j++) {
-            c->array[i][j] = (char *)malloc(sizeof(char) * 40);
-        }
     }
 
+    row = 0;
     while(fgets(texto, 100, f)) {
+        column = 0;
         
         texto[strlen(texto) - 1] = '\0';
-        //puts(texto);
         word = strtok(texto, ",");
 
         while(word) {
-            //puts(word);
+            int lengthOfWord = strlen(word);
+            c->array[row][column] = (char *)malloc(sizeof(char) * (lengthOfWord + 1));
             strcpy(c->array[row][column], word);
+            column++;
 
             word = strtok(NULL, ",");
-            column++;
         }
-        column = 0;
         row++;
     }
+    c->column = column;
+    c->row = row;
 }
 
 void printCSV(CSV csv){
-    for(int i = 0; i < 4; i++) {
-        for(int j = 0; j < 3; j++) {
+    for(int i = 0; i < csv.row; i++) {
+        for(int j = 0; j < csv.column; j++) {
             printf("%s, ", csv.array[i][j]);
         }
         printf("\b\b\n");
