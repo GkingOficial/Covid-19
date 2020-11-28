2318cc47ecaf1aca7181270005386b4e4544626e
Daniel-Alencar <danielalencar746@gmail.com>
2020-11-28 08:41:15 -0300

    melhor organização do código

--- Arvore binária.c
+++ Arvore binária.c
@@ -8,12 +8,22 @@ typedef struct node {
 }NODE;
 typedef NODE *BINARY_TREE;
 
+void makeTree(BINARY_TREE *tree, int value);
+void setLeft(BINARY_TREE tree, int value);
+void setRight(BINARY_TREE tree, int value);
+int value(BINARY_TREE tree);
+BINARY_TREE left(BINARY_TREE tree);
+BINARY_TREE right(BINARY_TREE tree);
+BINARY_TREE father(BINARY_TREE tree);
+BINARY_TREE brother(BINARY_TREE tree);
+int isLeft(BINARY_TREE tree);
+int isRight(BINARY_TREE tree);
+
 int main() {
     
 }
 
-void maketree(BINARY_TREE *tree, int value)
-{
+void makeTree(BINARY_TREE *tree, int value) {
     *tree = (BINARY_TREE) malloc (sizeof (NODE));
     if (!(*tree)) {
         printf("Erro! Nao existe memoria disponivel!");
@@ -23,8 +33,7 @@ void maketree(BINARY_TREE *tree, int value)
     (*tree)->left = (*tree)->father = (*tree)->right = NULL;
 }
 
-void setleft(BINARY_TREE tree, int value)
-{
+void setLeft(BINARY_TREE tree, int value) {
     tree->left = (BINARY_TREE) malloc (sizeof (NODE));
     if (!(tree->left)) {
         printf("Erro! Nao existe memoria disponivel!");
@@ -36,8 +45,7 @@ void setleft(BINARY_TREE tree, int value)
     tree->left->father = tree;
 }
 
-void setleft(BINARY_TREE tree, int value)
-{
+void setRight(BINARY_TREE tree, int value) {
     tree->right = (BINARY_TREE) malloc (sizeof (NODE));
     if (!(tree->right)) {
         printf("Erro! Nao existe memoria disponivel!");
@@ -65,20 +73,6 @@ BINARY_TREE father(BINARY_TREE tree) {
     return tree->father;
 }
 
-int isleft(BINARY_TREE tree) {
-    NODE *q = father(tree);
-    if (!q)
-        return (0);
-    if (left(q) == tree)
-        return (1);
-    return (0);
-}
-int isright(BINARY_TREE tree) {
-    if (father(tree))
-        return (!isleft(tree));
-    return (0);
-}
-
 BINARY_TREE brother(BINARY_TREE tree) {
     if (father(tree))
         if (isleft(tree))
@@ -86,4 +80,19 @@ BINARY_TREE brother(BINARY_TREE tree) {
         else
             return left(father(tree));
     return NULL;
+}
+
+int isLeft(BINARY_TREE tree) {
+    NODE *q = father(tree);
+    if (!q)
+        return (0);
+    if (left(q) == tree)
+        return (1);
+    return (0);
+}
+
+int isRight(BINARY_TREE tree) {
+    if (father(tree))
+        return (!isleft(tree));
+    return (0);
 }
